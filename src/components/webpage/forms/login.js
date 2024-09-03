52a5321531e2255dfae7127a4de53564659a82fa
aakriti <aakritip512@gmail.com>
2024-09-03 15:06:36 +0545

    updates

--- src/components/webpage/forms/login.js
+++ src/components/webpage/forms/login.js
@@ -30,7 +30,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://192.168.1.135:8000/api/accounts/login/', formData, {
+      const response = await axios.post('https://192.168.1.135:8000/api/accounts/login/', formData, {
         headers: {
           'Content-Type': 'application/json',
         },
